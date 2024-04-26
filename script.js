335058ba32fa90ef59ea6d6acb9c2900fcae9fc4
Guilherme <gui_araujo.carvalho@hotmail.com>
2024-04-26 09:47:39 -0300

    Corrigindo o erro

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ let p3=prompt('Digite o nome de um VILÃO')
 let p4=prompt("Digite um LUGAR")
 let msg=document.getElementById("msg")
 
-msg.innerHTML=`olá ${p1}, o seu poder é ${p2}e você vai enfrentar o(a) ${p3} no(em) ${p4}`
+msg.innerHTML=`olá ${p1}, o seu poder é ${p2} e você vai enfrentar o(a) ${p3} no(em) ${p4}`
